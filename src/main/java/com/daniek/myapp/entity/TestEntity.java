53eb2607f1df99f9bc818442f9b98698580c4485
DSerrob <dserrano@eglobal.com.mx>
2022-11-18 11:37:21 -0700

    Nombre del proyecto:my-app-daniel. Descripción: versión final del proyecto my-app-daniel

---
+++ src/main/java/com/daniek/myapp/entity/TestEntity.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.daniek.myapp.entity;
+
+/**
+ * @author Daniel Serrano
+ *
+ */
+public class TestEntity {
+	/**
+	 * Propiedad de prueba para la muestra de comilación del archivo testentity.
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
